857e61737bc5d3faa79fbca83f3aa6a4dcecec01
MadBorg <rednas@gmail.com>
2021-01-20 12:32:46 +0100

    added doc string to extract int

--- util.py
+++ util.py
@@ -1,2 +1,5 @@
-def extract_int(s) -> int:
+def extract_int(s: str) -> int:
+    """
+    Lopping over a string and extracts all chars where char.isdigit() is true
+    """
     return int("".join(digit for digit in s if digit.isdigit()))
